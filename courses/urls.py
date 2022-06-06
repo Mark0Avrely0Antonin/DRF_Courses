83c0797b513ef598fe9a00989ba2c2f8806a3850
Avror0973 <kozarakovca@gmail.com>
2022-06-06 07:05:30 +0300

    Changed my urls

--- courses/urls.py
+++ courses/urls.py
@@ -3,10 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # users/ LIST GET / CREATE POST
     path('courses/', ListAPICourses.as_view(), name='list_courses'),
     path('create_course/', CreateAPICourse.as_view(), name='create_course'),
-    # users/ID / DELETE /PATCH/PUT/ GET
     path('update_course/<int:pk>/', UpdateAPICourse.as_view(), name='update_course'),
 
     path('retrieve_course/<int:pk>/', RetrieveAPICourse.as_view(), name='retrieve_course'),
